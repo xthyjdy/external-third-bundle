e8395d95ad8ef94bb42051ec91211e69f7d138ca
vc <vc@togrund.de>
2026-01-06 16:21:48 +0100

    test

--- src/ExternalThirdBundle.php
+++ src/ExternalThirdBundle.php
@@ -4,20 +4,39 @@ namespace Eivorvch\ExternalThirdBundle;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class ExternalThirdBundle extends Bundle
+class ExternalThirdBundle extends Bundle //v2
 {
+    /**
+     * The build method is used to load the bundle's internal services.
+     */
     public function build(ContainerBuilder $container): void
     {
         parent::build($container);
+
+        // Path logic: assumes ExternalThirdBundle.php is in src/
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../config'));
+
         try {
             $loader->load('services.yaml');
         } catch (\Exception $e) {
-            // Log the error if the configuration fails to load.
             error_log('___my_err42: ' . $e->getMessage());
         }
+    }
+
+    /**
+     * Overriding this allows the bundle to be configured via config/packages/*.yaml
+     * without a separate DependencyInjection/ExternalThirdExtension.php file.
+     */
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        if (null === $this->extension) {
+            $this->extension = $this->createContainerExtension();
+        }
+
+        return $this->extension;
     }
 }
